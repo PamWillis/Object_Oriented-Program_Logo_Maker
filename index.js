91ff9b7e277581452837c246e00028dd03665470
Pam <pwillisop@yahoo.com>
2023-09-30 23:21:35 -0400

    rewrote shape and how generated

--- index.js
+++ index.js
@@ -1,73 +1,53 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle, Square, Triangle } = require("./lib/Shapes.js");
+const generateLogo = require("./lib/Shapes")
 
-class Svg {
-    constructor() {
-      this.textElement = "";
-      this.shapeElement = "";
-    }
-    render() {
-      return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeElement}${this.textElement}</svg>`;
-    }
-    settextElement(text, color) {
-      if (message.length > 3) {
-        throw new Error('Letters must not be more than 3 characters.');
-      }
-      else {
-        this.textElement = `<text x="150" y="100" font-size="40" text-anchor="middle" fill="${text - color}" >${text}</text>`;
-      }
-    }
-  
-    setshapeElement(Shape) {
-      this.shapeElement = Shape.render();
-    }
 
-  }
-const init =() => {
-inquirer
+
+
+const init = () => {
+  inquirer
     .prompt([
-        {
-            type: 'input',
-            name: 'text',
-            message: 'What are your initials?',
-        },
+      {
+        type: 'input',
+        name: 'text',
+        message: 'What are your initials?',
+      },
 
-        {
-            type: 'input',
-            name: 'text-color',
-            message: 'Choose a color for the initials',
-        },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Choose a color for the initials',
+      },
 
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a shape',
-            choices: ['circle', 'square', 'triangle']
-        },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape',
+        choices: ['circle', 'square', 'triangle']
+      },
 
-        {
-            type: 'input',
-            name: 'shape-color',
-            message: 'Enter a color keyword or hexadecimal number for the color of the shape',
-        },
-    ])
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color keyword or hexadecimal number for the color of the shape',
+      },
+    ]);
 
-    const filename = 'logo.svg'
-// Function to write data to file
-function writeToFile(Svg) {
-    filesystem.writeFile(fileName, Svg.render, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        else { 
-            console.log("Success SVG created!");
-        }
-    });
+  //writes file
+  const writeToFile = (filename, data) => {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  };
+
+  //initiate node app
+  const init = () => {
+    inquirer
+      .prompt(userInputs)
+      .then((answers) => {
+        writeToFile('logo.svg', generateLogo(answers));
+      });
+  }
 }
 
-}
-init();
-
-module.exports = index;
+module.exports = answers;
 
