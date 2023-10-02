849b78233900b3f9d986a8167b3e87646887278e
Pam <pwillisop@yahoo.com>
2023-10-02 15:13:56 -0400

    change3

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -1,14 +1,21 @@
-const Circle = require('../lib/Shapes.js');
+const { Circle }  = require("../lib/Shapes")
 
-//testing suite for Circle is created
-describe('Circle', () => {
-    //A test is created to check that a Circle is created when Circle is chosen
-    describe('Circle', () => {
-        it('should create a logo.svg', () => {
-            const Circle = 'Circle';
-            expect(generateLogo()).toEqual('Circle');           
-        });
+describe('shape', () => {
+    test('renders correctly', () => {
+      const shape = new Circle();
+      
+      var shapeColor = ('blue');
+      var textColor = ('red');
+      var text = ('RPW');
+      shape.setColor(shapeColor);
+      shape.settextColor(textColor);
+      shape.setText(text);
+      expect(shape.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />
+      <text x="150" y="112" font-size="40" text-anchor="middle" fill="${this.textColor}" >${this.text}</text>
+      </svg> `);
     });
-});
+  });
 
-//remove comment later
+
+//review
